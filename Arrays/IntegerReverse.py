44a46b0945d29af0f0a6ce9d0c07c727045beb5c
Shaun Brown <srb43@kent.ac.uk>
2022-12-19 09:59:16 +0000

    Reverse Integers

---
+++ Arrays/IntegerReverse.py
@@ -0,0 +1,36 @@
+"""
+Interview Question #3
+
+Our task is to design an efficient algorithm to reverse a given integer.
+For example if the input of the algorithm is 1234
+then the output should be 4321.
+
+"""
+
+
+# My Solution
+def reverse(i):
+    i = str(i)
+    return int(i[::-1])
+
+
+print(reverse(54321))
+
+# Tutor solution
+"""
+def reverse_integer(n):
+
+    reversed_integer = 0
+
+    while n > 0:
+        remainder = n % 10
+        reversed_integer = reversed_integer*10 + remainder
+        n = n // 10
+
+    return reversed_integer
+
+
+if __name__ == '__main__':
+    print(reverse_integer(12345678))
+
+"""
